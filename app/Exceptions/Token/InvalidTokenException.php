f67b0c218b35f336a7fca9982e6acb4bddfc17f7
frybasgames <frybasgames@gmail.com>
2024-09-04 13:54:11 +0300

    Change exception code

--- app/Exceptions/Token/InvalidTokenException.php
+++ app/Exceptions/Token/InvalidTokenException.php
@@ -3,6 +3,9 @@
 namespace App\Exceptions\Token;
 
 use App\Exceptions\AbstractException;
+use Illuminate\Http\Response;
 
 class InvalidTokenException extends AbstractException
-{}
+{
+    protected $code = Response::HTTP_GONE;
+}
--- app/Exceptions/Token/InvalidTokenFormatException.php
+++ app/Exceptions/Token/InvalidTokenFormatException.php
@@ -3,6 +3,9 @@
 namespace App\Exceptions\Token;
 
 use App\Exceptions\AbstractException;
+use Illuminate\Http\Response;
 
 class InvalidTokenFormatException extends AbstractException
-{}
+{
+    protected $code = Response::HTTP_GONE;
+}
