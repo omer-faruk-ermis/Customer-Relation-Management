0e67e9eee8eea128d87719f948b67ca43b48ed0f
frybasgames <frybasgames@gmail.com>
2024-09-10 16:01:27 +0300

    Change enum structure for bulk enums

--- app/Utils/ArrayUtil.php
+++ app/Utils/ArrayUtil.php
@@ -2,7 +2,7 @@
 
 namespace App\Utils;
 
-use Exception;
+use App\Enums\RegexPattern;
 use Illuminate\Support\Arr;
 
 /**
@@ -21,16 +21,36 @@ final class ArrayUtil
      */
     public static function castArray($value): string|array
     {
+        if (is_array($value)) return $value;
+
         if (is_string($value)) {
             $decodedValue = json_decode($value, true);
 
             if (json_last_error() === JSON_ERROR_NONE && is_array($decodedValue)) {
                 return $decodedValue;
-            } else {
-                return Arr::wrap($value);
             }
-        } else {
+            $value = self::stringArrayToArray($value);
+
             return Arr::wrap($value);
         }
+
+        return Arr::wrap($value);
+    }
+
+    /**
+     * Transform to real array value of array from kind of string.
+     *
+     * @param $value
+     *
+     * @return array|string
+     */
+    private static function stringArrayToArray($value): array|string
+    {
+        if (preg_match(RegexPattern::ARRAY_CHECK, $value)) {
+            $trimmedString = trim($value, '[]');
+            return preg_split(RegexPattern::COMMA_SPLIT, $trimmedString);
+        }
+
+        return $value;
     }
 }
